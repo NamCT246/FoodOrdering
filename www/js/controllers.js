89675742078f5d34cb1b63ce381e0696a84a75c8
NamCT <t4trna00@students.oamk.fi>
2017-01-25 17:58:43 +0200

    app overview

---
+++ www/js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers', [])
+
+.controller("LoginCtrl", function($scope, $state){
+  $scope.GoToSignup = function(){
+    $state.go('signup');
+  }
+  $scope.Recoverpw = function(){
+    $state.go('recoverpw');
+  }
+})
+
+.controller("SignupCtrl", function($scope, $state, $ionicHistory){
+  $scope.GoBack = function(){
+   $ionicHistory.nextViewOptions({
+          disableBack: true
+      });
+      $state.go('login');
+  }
+})
+.controller("RecoverpwCtrl",function($scope, $ionicHistory, $state){
+  $scope.GoBack = function(){
+   $ionicHistory.nextViewOptions({
+          disableBack: true
+      });
+      $state.go('login');
+  }
+});
